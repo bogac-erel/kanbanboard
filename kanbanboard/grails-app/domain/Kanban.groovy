17184450a087ce878d77dadb56ff8ab118d4b0dc
skye.roseboom <skye.roseboom@557fb200-c746-0410-8e38-498021e125fb>
2008-02-24 00:20:16 +0000

    Kanban.moveToStage(Stage stage, String user)

--- kanbanboard/grails-app/domain/Kanban.groovy
+++ kanbanboard/grails-app/domain/Kanban.groovy
@@ -4,9 +4,13 @@ class Kanban {
 	SortedSet stages
 	static hasMany = [stages:StageRecord]
 	
-	def getCurrentStage = {
+	def getCurrentStage() {
 		if (stages.isEmpty() == false){
 			return stages.first().stage
 		}
+	}
+	
+	def moveToStage(Stage stage, String user) {
+		this.addToStages(new StageRecord(stage: stage, user: user)).save();
 	}
 }
