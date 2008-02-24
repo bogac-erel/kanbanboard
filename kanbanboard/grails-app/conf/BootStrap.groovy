5bf23838b966341200fe3b57e09d48ab34a7e55c
skye.roseboom <skye.roseboom@557fb200-c746-0410-8e38-498021e125fb>
2008-02-24 00:04:08 +0000

    couple more pre-load kanban

--- kanbanboard/grails-app/conf/BootStrap.groovy
+++ kanbanboard/grails-app/conf/BootStrap.groovy
@@ -15,14 +15,20 @@ class BootStrap {
      	def readyStage = new Stage(name:"Ready", size:10, stageOrder: 1, color: "#FFFEBF").save();
      	new Stage(name:"Story Analysis", size:10, stageOrder: 2, color: "#FFB997").save();
      	new Stage(name:"Developement", size:25, stageOrder: 3, color: "#FF9797").save();
-     	new Stage(name:"Code/Peer Review", size:15, stageOrder: 4, color: "#EF97FF").save();
+     	def peerStage = new Stage(name:"Code/Peer Review", size:15, stageOrder: 4, color: "#EF97FF").save();
      	new Stage(name:"Walkthrough", size:10, stageOrder: 5, color: "#9797FF").save();
      	new Stage(name:"QA", size:20, stageOrder: 6, color: "#97E3FF").save();
      	new Stage(name:"Release Ready", size:10, stageOrder: 7, color: "#9EFF97").save();
      	
      	def firstKanban = new Kanban(name:"Kanban Board Crud", description:"Get the basic crud for the board working.").save()
+		def secondKanban = new Kanban(name:"Stage getKanbans", description:"Implement method to allows stages to get kanbans currently the state.").save()
+		def thirdKanban = new Kanban(name:"Second Ready", description:"Second kanban").save();
 		def firstStage = new StageRecord(user:"brad", kanban: firstKanban, stage: readyStage).save();
+		def secondStage = new StageRecord(user:"skye", kanban: secondKanban, stage: peerStage).save();
+		def thirdStage = new StageRecord(user:"skye", kanban: thirdKanban, stage: readyStage).save();
 		firstKanban.addToStages(firstStage).save()
+		secondKanban.addToStages(secondStage).save();
+		thirdKanban.addToStages(thirdStage).save();
      }
      def destroy = {
      }
