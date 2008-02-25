3d0d41087ac710608a5481400184897df467bdce
bmharris <bmharris@557fb200-c746-0410-8e38-498021e125fb>
2008-02-25 04:04:30 +0000

    removed commented out code

--- kanbanboard/grails-app/conf/BootStrap.groovy
+++ kanbanboard/grails-app/conf/BootStrap.groovy
@@ -1,17 +1,6 @@
 class BootStrap {
 
      def init = { servletContext ->
-     	/**
-     	new Stage(name:"Ready", size:10, stageOrder: 1).save();
-     	new Stage(name:"Story Analysis", size:10, stageOrder: 2).save();
-     	new Stage(name:"Developement", size:25, stageOrder: 3).save();
-     	new Stage(name:"Code/Peer Review", size:15, stageOrder: 4).save();
-     	new Stage(name:"Walkthrough", size:10, stageOrder: 5).save();
-     	new Stage(name:"QA", size:20, stageOrder: 6).save();
-     	new Stage(name:"Release Ready", size:10, stageOrder: 7).save();
-     	*/
-     	
-     	
      	def readyStage = new Stage(name:"Ready", size:10, stageOrder: 1, color: "#FFFEBF").save();
      	new Stage(name:"Story Analysis", size:10, stageOrder: 2, color: "#FFB997").save();
      	def devStage = new Stage(name:"Developement", size:25, stageOrder: 3, color: "#FF9797").save();
