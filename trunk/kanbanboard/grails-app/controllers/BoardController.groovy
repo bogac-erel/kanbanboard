80cd8d93051e2561bb878bae9082d56ddb7c02a2
skye.roseboom <skye.roseboom@557fb200-c746-0410-8e38-498021e125fb>
2008-03-02 07:29:19 +0000

    added flash message to KanbanController.moveToStage

--- trunk/kanbanboard/grails-app/controllers/BoardController.groovy
+++ trunk/kanbanboard/grails-app/controllers/BoardController.groovy
@@ -7,13 +7,15 @@ class BoardController {
 	def moveToStage = {
 		def kanban = Kanban.get( params.kanbanId );
 		def stage = Stage.get( params.stageId );
-		
+				
 		if(!kanban) {
             flash.message = "Kanban not found with id ${params.id}"
-            redirect(action:list)
-        }
-
-		kanban.moveToStage(stage, "moved");
+        } else if (!stage) {
+			flash.message = "Stage not found with id ${params.stageId}"
+		} else {
+			kanban.moveToStage(stage, "moved");
+			flash.message = "Kanban <b>${kanban.name}</b> moved to <b>${stage.name}</b>"	
+		}
 		redirect(action:index);
 	}
     
@@ -28,14 +30,14 @@ class BoardController {
 		if(!kanban.hasErrors() && kanban.save()) {
 			def stage = Stage.listOrderByStageOrder().get(0);
 			kanban.moveToStage(stage, "created").save()
-            flash.message = "Kanban ${kanban.name} created"
+            flash.message = "Kanban <b>${kanban.name}</b> created"
         }
         else {
 			String errorMsgs = "Error no kanban created"
 			kanban.errors.allErrors.each {
 				errorMsgs = errorMsgs + it
 			}
-            flash.mesage = errorMsgs
+            flash.message = errorMsgs
         }
         redirect(action:index)
     }
@@ -45,7 +47,7 @@ class BoardController {
         if(kanban) {
             kanban.setCurrentUser(params.user)
             if(!kanban.hasErrors() && kanban.save()) {
-                flash.message = "Kanban ${kanban.name} updated with owner ${params.user}"
+                flash.message = "Kanban <b>${kanban.name}</b> updated with owner <b>${params.user}</b>"
             }
 		} else {
 			flash.message = "Kanban not found with id ${params.id}"
